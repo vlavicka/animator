110ac30c46ea53cdb235b13c799f631ae61da5e6
Vladimir Lavicka <vladimir.lavicka@gmail.com>
2012-04-21 20:06:52 +0200

    Create base build script

--- build.cmd
+++ build.cmd
@@ -1,11 +1,22 @@
 @echo off
 setlocal
+
 call env.cmd
+pushd build
+cmake -G "MinGW Makefiles" --warn-uninitialized --warn-unused-vars -Wno-dev ..
+mingw32-make 
+rem mingw32-make test
+popd
 
-g++ -o test.exe test.cpp src\model\layer.cpp
+echo.
+echo -----------------------------------------------------------------------
+echo R U N N I N G   p y t h o n   r e g r e s s i o n   t e s t s
+echo -----------------------------------------------------------------------
+echo.
+pushd py
+call regr.cmd
+@echo off
+popd
 
-test.exe
-
-:end
 endlocal
 @echo on
--- env.cmd
+++ env.cmd
@@ -1,7 +1,7 @@
 if "%ENVDEFINED%" == "1" goto skip
 
 set PATH=c:\MinGW\bin;c:\develop\CMake_2.8\bin;c:\develop\swigwin-2.0.4;%PATH%
-set PYTHONPATH=%~d0%~p0
+set PYTHONPATH=%~d0%~p0\build
 
 set ENVDEFINED=1
 :skip
