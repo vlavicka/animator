47618be69621bbd961ff8f2d6b46bf9f756fa305
Vladimir Lavicka <vladimir.lavicka@gmail.com>
2012-04-23 17:08:50 +0200

    Move SWIG build into separate directory

--- env.cmd
+++ env.cmd
@@ -1,7 +1,7 @@
 if "%ENVDEFINED%" == "1" goto skip
 
 set PATH=c:\MinGW\bin;c:\develop\CMake_2.8\bin;c:\develop\swigwin-2.0.4;c:\Python27
-set PYTHONPATH=%~d0%~p0\build
+set PYTHONPATH=%~d0%~p0\build\swig
 
 set ENVDEFINED=1
 :skip
