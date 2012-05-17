d2790cad534d5934ee3062d95869261cb68513bb
Vladimir Lavicka <vlavicka@opentext.com>
2012-05-17 16:14:33 +0200

    Update tools paths

--- env.cmd
+++ env.cmd
@@ -1,8 +1,9 @@
 if "%ENVDEFINED%" == "1" goto skip
 
-set BOOST_ROOT=c:\develop\boost_1_49_0
+set OPENDEVDIR=c:\opendev
+set BOOST_ROOT=%OPENDEVDIR%\boost_1_49_0
 set BOOST_LIBRARYDIR=%BOOST_ROOT%\stage\lib
-set PATH=c:\MinGW\bin;c:\develop\CMake_2.8\bin;c:\develop\swigwin-2.0.4;c:\Python27;%BOOST_ROOT%;%BOOST_LIBRARYDIR%
+set PATH=%OPENDEVDIR%\MinGW\bin;%OPENDEVDIR%\CMake_2.8\bin;%OPENDEVDIR%\swigwin-2.0.6;c:\Python27;%BOOST_ROOT%;%BOOST_LIBRARYDIR%
 set PYTHONPATH=%~d0%~p0\build\%1\swig
 
 set ENVDEFINED=1
